e7c1640cc131ec92606d3a4f680183aa765b0299
okrylova2801 <o.krylova2801@gmail.com>
2021-05-23 21:30:04 +0600

    4lesson

---
+++ Lesson_4_Start_v_Codeception/tests/functional/SearchProductCest.php
@@ -0,0 +1,20 @@
+<?php
+
+class SearcheProductCest
+{
+    
+    /*
+    Проверка количества найденных товаров 
+    */
+    public function CheckArticlePageThroughList(FunctionalTester $I)
+    {
+        $I->amOnPage('index.php');
+        $I->seeElement('#search_query_top');
+        $I->fillField('#search_query_top', 'Printed dress');
+        $I->click('#searchbox > button');
+        $I->seeElement('.product_list');
+        $I->seeNumberOfElements('.product-container', 5);
+       
+        
+    }
+}
