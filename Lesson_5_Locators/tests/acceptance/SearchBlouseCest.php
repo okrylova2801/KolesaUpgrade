10b7ac7640c7616e2741b0155d8e85d5e4e17a2e
okrylova2801 <84120057+okrylova2801@users.noreply.github.com>
2021-05-27 10:57:32 +0600

    Update SearchBlouseCest.php

--- Lesson_5_Locators/tests/acceptance/SearchBlouseCest.php
+++ Lesson_5_Locators/tests/acceptance/SearchBlouseCest.php
@@ -24,10 +24,10 @@ class SearchBlouseCest
         $I->waitForElement('#homefeatured > li:nth-child(2) > div > div.left-block > div', 30);
         $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div');
         $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view > span');
-        // ожидание iframe с классом .fancybox-iframe в момент иниициализации модального окна (сожеражимое iframe отсутствует)
+        // ожидание iframe с классом .fancybox-iframe в момент иниициализации модального окна (содеражимое iframe отсутствует)
         $I->waitForElement('.fancybox-iframe', 30);
         $I->switchToIFrame('.fancybox-iframe');
-       //ожидание содержимого iframe
+        //ожидание содержимого iframe
         $I->waitForElement('#product', 30); 
         $I->waitForText('Blouse', 10, '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
 
