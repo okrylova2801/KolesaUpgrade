eefb980ec4c4bc8aa454c6e7b74e1be2a86fc0ff
okrylova2801 <84120057+okrylova2801@users.noreply.github.com>
2021-05-27 12:52:18 +0600

    Update SearchBlouseCest.php

--- Lesson_5_Locators/tests/acceptance/SearchBlouseCest.php
+++ Lesson_5_Locators/tests/acceptance/SearchBlouseCest.php
@@ -7,15 +7,19 @@ class SearchBlouseCest
      */
     public function checkSearchBlouse(AcceptanceTester $I)
     {
-
+        //'#homefeatured > li:nth-child(2) > div > div.left-block > div'
         $leftBlockDivCSS = '#homefeatured > li:nth-child(2) > div > div.left-block > div';
         $leftBlockDivXPath = '//*[@id="homefeatured"]/li[2]/div/div[1]/div';
+        //'#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view > span'
         $quickViewCSS = '#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view';
         $quickViewXPath = '//*[@id="homefeatured"]/li[2]/div/div[1]/div/a[2]';
+        //'.fancybox-iframe'
         $fancyboxIframeCSS = '.fancybox-iframe';
         $fancyboxIframeXPath = '//*[@class="fancybox-iframe"]';
+        //'#product'
         $productCSS = '#product';
         $productXPath = '//*[@id="product"]';
+        //'Blouse'
         $textBlouseCSS = '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1';
         $textBlouseXpath = '//*[@id="product"]/div/div/div[2]/h1';
 
