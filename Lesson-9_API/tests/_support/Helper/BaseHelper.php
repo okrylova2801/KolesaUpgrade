c7122e427d32939dfe3365dad60f4e8ba4a56d76
okrylova2801 <84120057+okrylova2801@users.noreply.github.com>
2021-06-09 21:51:32 +0600

    lesson9

---
+++ Lesson-9_API/tests/_support/Helper/BaseHelper.php
@@ -0,0 +1,17 @@
+<?php
+namespace Helper;
+
+use Faker\Factory;
+
+class BaseHelper extends \Codeception\Module
+{
+        public function getFaker()
+        {
+                $faker = Factory::create();
+                $faker->addProvider(new CustomFakerProvider($faker));
+
+                return $faker;
+        }
+        
+}
+ 
